dad0664de4d75eea8024f0ab4de64e8c2e097111
Alex Neustein <alex.neustein@gmail.com>
2018-05-08 18:20:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,9 @@ var katzDeli = [];
 
 function takeANumber(line,name) {
   /* This function adds the given name to the given deli line. */
-		  line.push(name);  // Add the given name to the next available index in the deli line array. 
+		  line.push(name);  // Add the given name to the next available index in the deli line array.
 		  var positionInLine = "Welcome, " + name + ". You are number " + (line.indexOf(name) + 1) + " in line.";
+      /* Create the string that confirms the position in line. */
 		}
 
 function nowServing() {
