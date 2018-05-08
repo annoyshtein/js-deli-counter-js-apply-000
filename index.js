daee2f38149b7787b07cc369d1f95e54c8ec5b79
Alex Neustein <alex.neustein@gmail.com>
2018-05-08 18:20:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ function takeANumber(line,name) {
 		  line.push(name);  // Add the given name to the next available index in the deli line array.
 		  var positionInLine = "Welcome, " + name + ". You are number " + (line.indexOf(name) + 1) + " in line.";
       /* Create the string that confirms the position in line. */
+      return positionInLine;
 		}
 
 function nowServing() {
